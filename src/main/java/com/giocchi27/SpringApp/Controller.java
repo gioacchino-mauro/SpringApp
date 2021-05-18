d8a02f7e48cee434ce7f329f2a97316320669cf9
gioacchino-mauro <81425201+gioacchino-mauro@users.noreply.github.com>
2021-05-18 10:43:44 +0200

    update controller

--- src/main/java/com/giocchi27/SpringApp/Controller.java
+++ src/main/java/com/giocchi27/SpringApp/Controller.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("test/api/v1")
 public class Controller {
 
 	private final EntityBL entityService;

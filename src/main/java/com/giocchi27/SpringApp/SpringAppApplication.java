29ed1d688762e021f1c4efe7f5e6a82e555cb2f1
Gioacchino Mauro <gioacchino.mauro@gpagroup.it>
2021-05-18 16:40:44 +0200

    console

--- src/main/java/com/giocchi27/SpringApp/SpringAppApplication.java
+++ src/main/java/com/giocchi27/SpringApp/SpringAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("STARTING APPLICATION...");
 		SpringApplication.run(SpringAppApplication.class, args);
 		System.out.println("RequestMapping = (\"api/v1\")");
 	}
